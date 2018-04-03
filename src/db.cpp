01b156d69e60f89d27a2677a89973f03e2e50ef9
Franz Schmidt <kinnarr@hollywuuud.de>
2018-04-03 10:53:24 +0200

    Addes history api

--- src/db.cpp
+++ src/db.cpp
@@ -2,6 +2,8 @@
 
 #include "log.hpp"
 
+#include <hta/ostream.hpp>
+
 Db::Db(const std::string& manager_host, const std::string& token)
 : dataheap2::Db(token), signals_(io_service, SIGINT, SIGTERM)
 {
@@ -36,4 +38,44 @@ void Db::data_callback(const std::string& metric_name, const dataheap2::DataChun
         metric->insert(TimeValue(tv));
     }
     metric->flush();
+}
+
+dataheap2::HistoryResponse Db::history_callback(const std::string& id,
+                                                const dataheap2::HistoryRequest& content)
+{
+    std::cerr << "History call\n";
+    dataheap2::HistoryResponse response;
+    response.set_metric(id);
+
+    auto metric = (*directory)[id];
+
+    auto start_ts = content.start_time();
+    auto end_ts = content.end_time();
+
+    // std::cerr << "Start " << std::ctime(&start_ts) << " End " << std::ctime(&end_ts) << "\n";
+
+    hta::TimePoint start_time(hta::duration_cast(std::chrono::nanoseconds(content.start_time())));
+    hta::TimePoint end_time(hta::duration_cast(std::chrono::nanoseconds(content.end_time())));
+    auto interval_ns = hta::duration_cast(std::chrono::nanoseconds(content.interval_ns()));
+
+    std::cerr << "Start " << start_time << " end " << end_time << " intervl " << interval_ns.count()
+              << "\n";
+
+    auto rows = metric->retrieve(start_time, end_time, interval_ns);
+    std::cerr << "Rows count " << rows.size() << " raw " << metric->count(start_time, end_time)
+              << "\n";
+    hta::TimePoint last_time;
+
+    for (auto row : rows)
+    {
+        auto time_delta =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(row.time - last_time);
+        response.add_time_delta(time_delta.count());
+        response.add_value_min(row.aggregate.minimum);
+        response.add_value_max(row.aggregate.maximum);
+        response.add_value_avg(row.aggregate.mean());
+        last_time = row.time;
+    }
+
+    return response;
 }
--- src/db.hpp
+++ src/db.hpp
@@ -3,6 +3,7 @@
 #include <dataheap2/db.hpp>
 
 #include <dataheap2/datachunk.pb.h>
+#include <dataheap2/history.pb.h>
 
 #include <hta/directory.hpp>
 #include <hta/hta.hpp>
@@ -13,11 +14,10 @@
 
 using json = nlohmann::json;
 
-
 struct TimeValue
 {
     TimeValue(dataheap2::TimeValue dtv)
-            : htv{ hta::TimePoint(hta::duration_cast(dtv.time.time_since_epoch())), dtv.value }
+    : htv{ hta::TimePoint(hta::duration_cast(dtv.time.time_since_epoch())), dtv.value }
     {
     }
 
@@ -35,6 +35,8 @@ public:
     Db(const std::string& manager_host, const std::string& token = "htaDb");
 
 private:
+    dataheap2::HistoryResponse history_callback(const std::string& id,
+                                                const dataheap2::HistoryRequest& content) override;
     void db_config_callback(const json& config) override;
     void ready_callback();
     void data_callback(const std::string& metric_name, const dataheap2::DataChunk& chunk) override;
