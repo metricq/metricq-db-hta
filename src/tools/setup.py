96e09f67cdcdabe246f62f1da13b84e963f174d0
Mario Bielert <mario.bielert@tu-dresden.de>
2019-04-08 12:43:29 +0200

    Adds dataheap importer

---
+++ src/tools/setup.py
@@ -0,0 +1,33 @@
+# metricq
+# Copyright (C) 2019 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+#
+# All rights reserved.
+#
+# This file is part of metricq.
+#
+# metricq is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# metricq is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with metricq.  If not, see <http://www.gnu.org/licenses/>.
+from setuptools import setup
+
+setup(name='metricq_importer',
+      version='0.1',
+      author='TU Dresden',
+      python_requires=">=3.7",
+      packages=['metricq_importer'],
+      scripts=[],
+      entry_points='''
+      [console_scripts]
+      metricq-importer=metricq_importer:importer_cmd
+      ''',
+      install_requires=['click', 'click-completion', 'click-log', 'metricq', 'cloudant'],
+      )
